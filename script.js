5bbeaa53ef0553a405e7a8a9f5a6176c8b5cb044
Lida12345 <lidagatalskay@gmail.com>
2021-06-06 20:22:20 +0300

    modified file

--- script.js
+++ script.js
@@ -277,10 +277,37 @@ const q = {
 
 const newObj = {...q};
 
+// Урок 23
 
+let str = "some";
+let strObj = new String(str);
 
+//console.log(typeof(str));
+//console.log(typeof(strObj));
 
+console.dir([1,2,3]);
 
+const soldier = {
+    health: 400,
+    armor: 100,
+    sayHello: function() {
+        console.log("Hello");
+    }
+};
+
+const jonh = Object.create(soldier);
+
+/*const jonh = {
+    health:100
+};*/
+
+//jonh.__proto__ = soldier; //устаревший формат
+
+//Object.setPrototypeOf(jonh, soldier);
+
+//console.log(jonh.armor);
+
+jonh.sayHello();
 
 
 
